a20b66f3e468592b76b9ad897963b6714b9a2b89
Shady Emad <Shadygirgis79@gmail.com>
2024-08-08 16:37:43 +0300

    Add files via upload

---
+++ Migrations/20231231080340_Latest_Version.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class Latest_Version : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureURl",
+                table: "Producers",
+                newName: "ProfilePictureURL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureURL",
+                table: "Producers",
+                newName: "ProfilePictureURl");
+        }
+    }
+}
---
+++ Models/Cinema.cs
@@ -0,0 +1,28 @@
+﻿using eTickets.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Cinema : IEntityBase
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name ="Cinema Logo")]
+        [Required(ErrorMessage = "Cinema logo is required")]
+        public string Logo { get; set; }
+        [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Cinema Name is required")]
+        public string Name { get; set; }
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        //relationships
+        public List<Movie> Movies { get; set; }
+    }
+}
